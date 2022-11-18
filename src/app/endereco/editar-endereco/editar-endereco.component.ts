e5424319132c284885e3d2c64c5e4e7f967f602a
Felipe <felipeavzampa90@gmail.com>
2022-11-18 14:11:35 -0300

    crud endereco OK

--- src/app/endereco/editar-endereco/editar-endereco.component.ts
+++ src/app/endereco/editar-endereco/editar-endereco.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Endereco } from 'src/app/shared/models/endereco.model';
+import { EnderecoService } from '../services/endereco.service';
 
 @Component({
   selector: 'app-editar-endereco',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-endereco.component.css']
 })
 export class EditarEnderecoComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild("formEndereco") formendereco!: NgForm;
+  endereco!: Endereco;
+  constructor(private enderecoService: EnderecoService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.params['id'];
+    const res = this.enderecoService.buscarPorId(id);
+    if (res !== undefined) {
+      this.endereco = res;
+    } else {
+      throw new Error("endereco não encontrada: id = " + id);
+    }
   }
 
+  atualizar(): void {
+    // Verifica se o formulário é válido
+    if (this.formendereco.form.valid) {
+      // Efetivamente atualiza a endereco
+      this.enderecoService.atualizar(this.endereco);
+      // Redireciona para /enderecos/listar
+      this.router.navigate(['/enderecos']);
+    }
+  }
 }
